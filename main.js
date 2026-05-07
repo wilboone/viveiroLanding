3227f0d000594fffb40b22ec30db4db37fd9adf4
Wilson Boone <wilson.boone@gmail.com>
2026-05-07 15:11:59 -0400

    ajuste responsiv e api

--- main.js
+++ main.js
@@ -17,3 +17,26 @@ close.addEventListener('click', function (e) {
   info.classList.add('hidden');
   overlay.classList.add('hidden');
 });
+
+overlay.addEventListener('click', function (e) {
+  e.preventDefault();
+  info.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+/// mapa
+
+/// -16.573901, -39.583073
+const latitude = -16.573901;
+const longitude = -39.583073;
+
+const mapa = L.map('mapa').setView([latitude, longitude], 15);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors',
+}).addTo(mapa);
+
+L.marker([latitude, longitude])
+  .addTo(mapa)
+  .bindPopup('Nossa localização')
+  .openPopup();
